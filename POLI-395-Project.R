bf8a7665216783bdcbeada51ba82c21304b36704
jasirrahman <51933869+jasirrahman@users.noreply.github.com>
2023-10-26 20:59:03 -0500

    Update POLI-395-Project.R
    loaded in all data

--- POLI-395-Project.R
+++ POLI-395-Project.R
@@ -1,19 +1,20 @@
 ##Poli 395 Project
 ##08/28/23
-#JKR
+#Jasir Rahman
 
 ####################
 
-##clear environment
+##setup
+
+#clear environment
 rm(list=ls())  
+
 #setwd
 setwd("~/Desktop/R/POLI 395/POLI 395 Project")
+
 #libraries + packages
 library(devtools)
 library(foreign)
-#install a package from GitHub
-
-#you may need to allow R to update/install additional packages
 library(qss)
 library(ggplot2)
 library(dplyr)
@@ -21,13 +22,16 @@ library(stargazer)
 
 ##Load in data
 lobby <- read.csv('lobbying_data_full_2020.csv')
-lobby_filtered <- lobby[, c(3,5,7,8,9,10,15,16,17,20,22,23,25,26,29,30,31,33)]
-leg <- read.csv('')
+leg_116 <- read.csv('house_legislation_116.csv')
+mem_116 <- read.csv('house_members_116.csv')
+roll_info_116 <- read.csv('house_rollcall_info_116.csv')
+roll_vote_116 <- read.csv('house_rollcall_votes_116.csv')
 
+#filter out unnecessary columns
+lobby_filtered <- lobby[, c(3,5,7,8,9,10,15,16,17,20,22,23,25,26,29,30,31,33)]
 target_string = 'Firearms/Guns/Ammunition'
 gv <- lobby_filtered %>% 
   filter(str_detect(general_issues, target_string))
-
 
 
 
